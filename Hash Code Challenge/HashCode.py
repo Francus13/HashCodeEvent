cb4418a51637056c32aa7bba264ef45b727a0ee2
Francus13 <francis.rinaldi@gmail.com>
2020-02-20 13:58:44 -0600

    Simple Skeleton

--- Hash Code Challenge/HashCode.py
+++ Hash Code Challenge/HashCode.py
@@ -1,24 +1,39 @@
-#Initializing data
-with open('a_example.txt', 'r') as f:
-    data = f.read()
-o = open('output.txt', 'w')
 
-lines = data.split('\n')
+num_libs = 0
+all_books = []
+
+class Library:
+    def __init__(self, books, days_to_screen, books_per_day):
+        self.books = books
+        self.days_to_screen = days_to_screen
+        self.books_per_day = books_per_day
+
+    def get_output(self):
+        return min()
+
+    def rembook(self, id):
+        self.books.remove(id)
+
+
+# reading file
+with open('a_example.txt', 'r') as f:
+    input = f.read()
+
+
+lines = input.split('\n')
 
 totalBooks = lines[0][0]
 totalLibraries = lines[0][1]
 totalDays = lines[0][2]
 
 answer = ''
-#compute scores
-def computeLibraryScore(l,d):
-    
-# ALL ALGORITHM WORK GOES HERE
 
 libraryScore = computeLibraryScore(library, daysAvailable)
 
 
 #writing the output file
+o = open('output.txt', 'w')
+
 o.write(answer)
 f.close()
 o.close()
